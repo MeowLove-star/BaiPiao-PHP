dc17df5d5c18480dd2bf06830f30955aa82258db
landlord1 <940780482@qq.com>
2019-07-11 10:51:53 +0800

    国豪
    Log文件夹

--- index.php
+++ index.php
@@ -2,19 +2,24 @@
 // +----------------------------------------------------------------------
 // | ThinkPHP [ WE CAN DO IT JUST THINK ]
 // +----------------------------------------------------------------------
-// | Copyright (c) 2006~2018 http://thinkphp.cn All rights reserved.
+// | Copyright (c) 2006-2016 http://thinkphp.cn All rights reserved.
 // +----------------------------------------------------------------------
 // | Licensed ( http://www.apache.org/licenses/LICENSE-2.0 )
 // +----------------------------------------------------------------------
 // | Author: liu21st <liu21st@gmail.com>
 // +----------------------------------------------------------------------
 
-use think\Route;
-// restful 路由列子
-// Route::get('api/:version/Category','api/:version.Category/getAllCategory');
+// [ 应用入口文件 ]
 
+// 定义应用目录
+define('APP_PATH', __DIR__ . '/../application/');
+define('LOG_PATH', __DIR__ . '/../log/');
+// 加载框架引导文件
+require __DIR__ . '/../thinkphp/start.php';
 
-//http://localhost/BaiPiao-PHP/public/api/v1/Login   (访问方法)
- Route::post('api/:version/Login','api/:version.Login/login');
-
-
+\think\Log::init([
+    'type'=>'File',
+    'path'=>LOG_PATH,
+    'level'=>['sql'],
+    'max_files'	=> 10,
+]);
