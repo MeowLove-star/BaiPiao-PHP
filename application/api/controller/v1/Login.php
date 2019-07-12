b647cc9632ee7ac004601f9e7a2e3a13ae0ef16d
landlord1 <940780482@qq.com>
2019-07-12 16:51:26 +0800

    跨域header

--- application/api/controller/v1/Login.php
+++ application/api/controller/v1/Login.php
@@ -3,6 +3,12 @@ namespace app\api\controller\v1;
 use app\lib\exception\ApiException;
 use app\api\validate\LoginCheck;
 use app\api\model\User;
+// 制定允许其他域名访问
+header("Access-Control-Allow-Origin:*");
+// 响应类型
+header('Access-Control-Allow-Methods:POST');
+// 响应头设置
+header('Access-Control-Allow-Headers:x-requested-with, content-type');
 class Login{
     public function login(){
         if(request()->isPost()){
